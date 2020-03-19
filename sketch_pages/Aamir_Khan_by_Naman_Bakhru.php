c8cb463543472cb0f427a40d94c799f1f4694a52
Naman Bakhru <namanbakhru@gmail.com>
2020-03-19 21:00:40 +0530

    Added JQuery

---
+++ sketch_pages/Aamir_Khan_by_Naman_Bakhru.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<!--<link rel="shortcut icon" type="image/x-icon" href="">-->
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>Char Sketch</title>
+<link rel="stylesheet" type="text/css" media="screen and (orientation:portrait)" href="css/common_skch_portrait.css">
+<link rel="stylesheet" type="text/css" media="screen and (orientation:landscape)" href="css/common_skch_landscape.css">
+</head>
+<body>
+<h2 id="skchtitle">Mr. Perfectionist</h2>
+<div id="skch">An amazing actor. I wish I could meet him once.
+
+His acting is so perfect that it becomes impossible to describe is character. Needless to say, he is a humble person.
+</div>
+<script type="text/javascript">
+var flag =;
+var filename =;
+var sharetoken =;
+</script>
+</body>
+</html>
